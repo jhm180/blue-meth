7cbe86318a46f9d98fca149fd8e2393096b9d4a8
Oliver Mellet <omellet@gmail.com>
2019-01-22 22:27:41 -0500

    added newcron

--- manual_run.sh
+++ manual_run.sh
@@ -3,15 +3,20 @@ source ~/env/pydata/bin/activate
 export PYTHONPATH=$PYTHONPATH:/home/oliver/Dropbox/whitepine/lib;
 export PATH=/home/oliver/bin:/home/oliver/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/go/bin:/home/oliver/src/julia/usr/bin:/home/oliver/bin:/home/oliver/android_sdk/platform-tools:/home/oliver/android_sdk/tools:/usr/local/go/bin:/home/oliver/src/julia/usr/bin:/home/oliver/bin:/home/oliver/android_sdk/platform-tools:/home/oliver/android_sdk/tools:/usr/local/go/bin:/home/oliver/src/julia/usr/bin:/home/oliver/bin:/home/oliver/android_sdk/platform-tools:/home/oliver/android_sdk/tools
 export PYTHONPATH=$PYTHONPATH:/home/oliver/Dropbox/whitepine/lib;
-date
+#/home/oliver/bin/wpcmd import
 ulimit -d unlimited
 ulimit -m unlimited
 ulimit -s unlimited
-/home/oliver/bin/wpcmd import
-#python /home/oliver/src/blue-meth/ipynb/rapnet_loader.py
+CONTINUE=1
+#while [ $CONTINUE != 0 ]
+#do
+#   date
+#   echo "Running loader"
+#   python /home/oliver/src/blue-meth/ipynb/rapnet_loader.py
+#   CONTINUE=$? # 
+#done
 date
 #python /home/oliver/Dropbox/whitepine/lib/cron.py
 #date
-#python /home/oliver/Dropbox/whitepine/lib/newcron.py
-python /home/oliver/src/blue-meth/ipynb/newcron.py
+python /home/oliver/Dropbox/whitepine/lib/newcron.py
 date
