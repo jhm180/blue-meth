1c4fe322d3ad0bdc5ba487521110fbb886ccff15
Joe Mellet <joe.mellet@gmail.com>
2019-01-25 17:19:34 -0500

    harden rap dl func - now triggered by pubsub

--- cloudfunctions/RapDownload/main.py
+++ cloudfunctions/RapDownload/main.py
@@ -2,32 +2,29 @@ import http.client, urllib
 from google.cloud import storage
 import os
 from os import path
-import base64
 import logging
 import sys 
 import datetime
 
-def get_rap_file(request):
-	# authenticate with Rap server and get ticket 
-	creds = urllib.parse.urlencode({'username': '73906', 'password': 'Certs5355'})
-	headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-	conn = http.client.HTTPSConnection("technet.rapaport.com")
-	conn.request("POST", "/HTTP/Authenticate.aspx", creds, headers)
-	response = conn.getresponse()
-	auth_ticket = response.read()
-	conn.close()
 
-	y = datetime.date.today().strftime("%Y")
-	m = datetime.date.today().strftime("%m")
-	d = datetime.date.today().strftime("%d")
-	destination_blob_name = y+'-'+m+'-'+d+'-'+'FullRapFile.csv'
+def get_rap_file_v2(event,callback):
+	try: 
+		# authenticate with Rap server and get ticket 
+		creds = urllib.parse.urlencode({'username': '73906', 'password': 'Certs5355'})
+		headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+		conn = http.client.HTTPSConnection("technet.rapaport.com")
+		conn.request("POST", "/HTTP/Authenticate.aspx", creds, headers)
+		response = conn.getresponse()
+		auth_ticket = response.read()
+		conn.close()
 
-	bucket_name = 'rapdvtfiles'
-	source_file_name = '/tmp/download.csv'
-	# destination_blob_name = 'upload.csv'
-	
-	try:
-		# TO DO - Update Filename to include date, upload 
+		y = datetime.date.today().strftime("%Y")
+		m = datetime.date.today().strftime("%m")
+		d = datetime.date.today().strftime("%d")
+		destination_blob_name = y+'-'+m+'-'+d+'-'+'FullRapFile.csv'
+		bucket_name = 'rapdvtfiles'
+		source_file_name = '/tmp/download.csv'
+		
 		# TO DO - email joe if error
 		
 		# get file from Rap
@@ -48,9 +45,21 @@ def get_rap_file(request):
 		blob = bucket.blob(destination_blob_name)
 		blob.upload_from_filename(source_file_name)
 		os.close(output_file)
-		logging.warn('output file size - ' + str(sys.getsizeof(output_file)))
-		return 'success ' + str(destination_blob_name)
+	except Exception:
+		return "Failed to download file from Rap and upload to cloud bucket"
 
-	except IOError:
-		return 'fail ' + str(destination_blob_name)
+'''
+	try: 
+		logging.warn('pre auth_key')
+		logging.warn('Auth Key type = '+str(type(request.json["auth_key"])))
+		auth_key = request.json["auth_key"]
+		logging.warn('Auth Key = '+str(auth_key))
+		if auth_key != "carsliftdoorhead":
+			return  "Invalid authorization key"
+		else: 
+'''
+#	except Exception: 
+#		return "JSON body is invalid. JSON must include key value pair for {auth_key : key}"
+
+
 
